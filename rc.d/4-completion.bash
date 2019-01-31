c918d6320202abd9bdf876840aa57f7018946820
Rene Gielen <rgielen@apache.org>
2019-01-31 13:51:46 +0100

    add proper brew completion include

--- rc.d/4-completion.bash
+++ rc.d/4-completion.bash
@@ -1,14 +1,14 @@
-# Add tab completion for many more commands in Homebrew
-#if [ -f $(brew --prefix)/etc/bash_completion ]; then
-#    . $(brew --prefix)/etc/bash_completion
-#fi
-if [ -f /usr/local/etc/bash_completion ]; then
-    . /usr/local/etc/bash_completion
-fi
+if [ $(uname) = "Darwin" ] && command -v brew &>/dev/null ; then
+  BREW_PREFIX=$(brew --prefix)
 
-# From installing brew bash-completion@2:
-if [ -f /usr/local/share/bash-completion/bash_completion ]; then
-    . /usr/local/share/bash-completion/bash_completion
+  if [ -f "$BREW_PREFIX"/etc/bash_completion ]; then
+    . "$BREW_PREFIX"/etc/bash_completion
+  fi
+
+ # homebrew/versions/bash-completion2 (required for projects.completion.bash) is installed to this path
+  if [ "${BASH_VERSINFO}" -ge 4 ] && [ -f "$BREW_PREFIX"/share/bash-completion/bash_completion ]; then
+    . "$BREW_PREFIX"/share/bash-completion/bash_completion
+  fi
 fi
 
 # Add kubectl completion, not needed when usding brew completion
