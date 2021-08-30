6fe8fb7916e21f1412b762bd0bf5d427cfd32630
René Gielen <rgielen@apache.org>
2021-08-30 18:19:28 +0200

    Add proper brew initialization

--- rc.d/1-path.bash
+++ rc.d/1-path.bash
@@ -1,3 +1,6 @@
+[ -x "/opt/homebrew/bin/brew" ] && eval $(/opt/homebrew/bin/brew shellenv)
+[ -x "/usr/local/bin/brew" ] && eval $(/usr/local/bin/brew shellenv)
+
 # Homebrew sbin
 if [ -d /usr/local/sbin ]; then
     export PATH=/usr/local/sbin:$PATH
