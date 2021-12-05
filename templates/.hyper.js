b216b9c3446fa4a7359b296d132582b27c5f9551
René Gielen <rgielen@apache.org>
2021-12-05 11:44:55 +0100

    Add hyper plugins

--- templates/.hyper.js
+++ templates/.hyper.js
@@ -134,7 +134,12 @@ module.exports = {
     //   `hyperpower`
     //   `@company/project`
     //   `project#1.0.1`
-    plugins: [],
+    plugins: [
+    	"hypercwd",
+		"hyper-search",
+		"hyper-pane",
+		"hyper-highlight-active-pane",
+    ],
     // in development, you can create a directory under
     // `~/.hyper_plugins/local/` and include it here
     // to load it and avoid it being `npm install`ed
