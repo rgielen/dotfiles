a1d832651add7fe5190c258fa0cb8b02c7c0ef87
Rene Gielen <rgielen@apache.org>
2021-12-11 11:44:58 +0100

    Add linuxbrew support

--- rc.d/1-path.bash
+++ rc.d/1-path.bash
@@ -1,5 +1,6 @@
 [ -x "/opt/homebrew/bin/brew" ] && eval $(/opt/homebrew/bin/brew shellenv)
 [ -x "/usr/local/bin/brew" ] && eval $(/usr/local/bin/brew shellenv)
+[ -x "/home/linuxbrew/.linuxbrew/bin/brew" ] && eval $(/home/linuxbrew/.linuxbrew/bin/brew)
 
 # Fabric8
 if [ -d $HOME/.fabric8/bin ]; then
