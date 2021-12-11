75b61513d23f50784e765b8ebbe7e6ec72f8d2d7
Rene Gielen <rgielen@apache.org>
2021-12-11 11:45:53 +0100

    Fix problem with linuxbrew script

--- rc.d/1-path.bash
+++ rc.d/1-path.bash
@@ -1,6 +1,6 @@
 [ -x "/opt/homebrew/bin/brew" ] && eval $(/opt/homebrew/bin/brew shellenv)
 [ -x "/usr/local/bin/brew" ] && eval $(/usr/local/bin/brew shellenv)
-[ -x "/home/linuxbrew/.linuxbrew/bin/brew" ] && eval $(/home/linuxbrew/.linuxbrew/bin/brew)
+[ -x "/home/linuxbrew/.linuxbrew/bin/brew" ] && eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
 
 # Fabric8
 if [ -d $HOME/.fabric8/bin ]; then
