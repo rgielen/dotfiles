d4013c1c53b46b5808853a72542f6c46cc9e9dcd
René Gielen <rgielen@apache.org>
2024-01-08 06:39:52 +0100

    Add mcfly init

--- scripts/zsh.final.d/zsh-final-setup.zsh
+++ scripts/zsh.final.d/zsh-final-setup.zsh
@@ -2,6 +2,11 @@ if command -v direnv &> /dev/null; then
     eval "$(direnv hook bash)"
 fi
 
+if command -v mcfly &> /dev/null; then 
+    export MCFLY_RESULTS_SORT=LAST_RUN
+    eval "$(mcfly init zsh)"
+fi
+
 if command -v switcher &> /dev/null; then 
     source <(switcher init zsh)
     alias s=switch
